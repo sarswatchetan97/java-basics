72c420f23cb00b4a62d52102beb95671822f685b
Chetan Sarswat <sarswatchetan97@gmail.com>
2026-03-21 00:09:56 +0530

    Stream Intermediate and Terminal Operations

---
+++ src/StreamExample2.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamExample2 {
+    public static void main(String[] args) {
+
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 3, 4, 5, 5, 6, 7, 8, 9, 10);
+
+        List<Integer> squaredIntegers = numbers.stream()
+                .filter(n -> n % 2 == 0)
+                .map(n -> n * n)
+                .distinct()
+                .peek(System.out::println)
+                .collect(Collectors.toList());
+
+        System.out.println("Square of Even Number: " + squaredIntegers);
+
+        long count = numbers.stream()
+                .filter(n -> n > 5)
+                .distinct()
+                .count();
+
+        System.out.println("Count of numbers: " + count);
+
+        int sum = numbers.stream()
+                .mapToInt(Integer::intValue)
+                .sum();
+
+        System.out.println("Sum of Numbers: " + sum);
+    }
+}

871c83616adfa111b110a7199811ec34bca2f508
Chetan Sarswat <sarswatchetan97@gmail.com>
2026-04-09 22:49:56 +0530

    Rectangle Solid Pattern

--- src/SolidPattern.java
+++ src/SolidPattern.java
@@ -1,9 +1,18 @@
 public class SolidPattern {
     public static void main(String[] args) {
-        int n = 4;
-
+        // Square Solid Pattern
+        /*int n = 4;
         for (int row=1; row<=n; row++) {
             for (int col=1; col<=n; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }*/
+
+        // Rectangular Solid Pattern
+        int n = 3;
+        for (int row=1; row<=n; row++) {
+            for (int col=1; col<=5; col++) {
                 System.out.print("* ");
             }
             System.out.println();

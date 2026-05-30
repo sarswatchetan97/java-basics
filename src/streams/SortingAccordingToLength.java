8f0457445f50fb46aa2bff9821eeeff7d6135104
Chetan Sarswat <sarswatchetan97@gmail.com>
2026-05-30 22:01:32 +0530

    Sorting according to String length using Stream

--- src/streams/SortingAccordingToLength.java
+++ src/streams/SortingAccordingToLength.java
@@ -1,4 +1,17 @@
 package streams;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
 public class SortingAccordingToLength {
+    public static void main(String[] args) {
+        List<String> stringList = Arrays.asList("Java", "Python", "C#", "HTML", "Kotlin", "C++",
+                "COBOL", "C");
+
+        List<String> result = stringList.stream()
+                .sorted(Comparator.comparing(String::length)).toList();
+
+        System.out.println(result);
+    }
 }

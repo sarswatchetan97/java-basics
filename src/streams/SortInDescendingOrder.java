4465f0eca2319520626c8446b87d100ac126e106
Chetan Sarswat <sarswatchetan97@gmail.com>
2026-05-30 21:52:11 +0530

    Sorting in descending order

--- src/streams/SortInDescendingOrder.java
+++ src/streams/SortInDescendingOrder.java
@@ -1,4 +1,16 @@
 package streams;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
 public class SortInDescendingOrder {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(11, 11, 1, 3, 5, 6, 5);
+        List<Integer> result = numbers.stream()
+                .distinct()
+                .sorted(Comparator.reverseOrder()).toList();
+
+        System.out.println(result);
+    }
 }
